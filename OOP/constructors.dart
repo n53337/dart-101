0d4d88a071936cf14ff3893f9a75896ff7584fff
u53f <scalewithusef@gmail.com>
2023-07-12 12:18:01 +0100

    edit: constructor

--- OOP/constructors.dart
+++ OOP/constructors.dart
@@ -1,23 +1,28 @@
 void main(){
-
+  Person p1 = Person.create(isAllowed: true, name: "usef", age: 21);
 }
 
 class Person {
-  final int age;
-  final String name;
-  final bool isAllowed;
+   int? age;
+   String? name;
+   bool? isAllowed;
 
   // Default Constructor
   Person(this.age, this.isAllowed, [this.name = 'nobody']);
+  Person.create({required bool isAllowed, required String name, required int age}){
+    this.isAllowed = isAllowed;
+    this.name = name;
+    this.age = age;
+  }
 
   // Named Constructor
-  Person.create(int age, String name, bool isAllowed){
-    this.age = age;
-    this.name = name;
-    this.isAllowed = isAllowed;
-  }
+  // Person.create(int age, String name, bool isAllowed){
+  //   this.age = age;
+  //   this.name = name;
+  //   this.isAllowed = isAllowed;
+  // }
 
   // Const Constructor
 
-  const Person(this.age, this.name, this.isAllowed);
+  // const Person(this.age, this.name, this.isAllowed);
 }
